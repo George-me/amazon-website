6d482a992afd77c20eebcd4a9d88c07a5fb38a43
George Mekhael <georgexp@live.com>
2024-09-26 22:47:00 +0400

    Initialising the project

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      {/* Top navigation container */}
+      <div>top</div>
+
+      {/* Bottom navigation container */}
+      <div>bottom</div>
+    </header>
+  );
+};
+
+export default Header;

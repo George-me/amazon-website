f23cb35e92a8000a4f2f2624e6364b867e39eb67
George Mekhael <georgexp@live.com>
2024-10-06 13:30:23 +0400

    Finalised top navigation bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
 import amazonLogo from "../app/assets/amazon_logo_white.png";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+  MapPinIcon,
+} from "@heroicons/react/24/outline";
 
 const Header = () => {
   return (
     <header>
       {/* Top navigation container */}
-      <div className="flex items-center bg-amazon_blue">
-        <div className="mt-1.5 px-1.5">
+      <div className="flex justify-between items-center bg-amazon_blue">
+        <div className="mt-1.5 px-1.5 flex-shrink-0">
           <Image
             alt="amazon logo"
             src={amazonLogo}
-            // width={150}
             height={40.5}
-            className="cursor-pointer m-1.5 pt-0.5 pr-2 pl-1.5"
+            className="cursor-pointer m-1.5 pt-[6px] pr-2 pl-1.5"
           />
         </div>
 
         {/* Search bar */}
-        <div className="flex flex-grow focus-within:bg-amazon_search_focus-focus rounded-lg p-[3px]">
+        <div className="hidden md:flex flex-grow focus-within:bg-amazon_search_focus-focus rounded-lg p-[3px]">
           <input
             type="text"
             className="h-10 flex-grow rounded-l-md focus:outline-none px-4"
@@ -27,6 +30,27 @@ const Header = () => {
           <button className="btn bg-yellow-600 rounded-r-md text-white px-0.5">
             <MagnifyingGlassIcon className="h-10 p-2" />
           </button>
+        </div>
+
+        {/* Right section */}
+        <div className="flex items-center text-white text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello George Mekhael </p>
+            <p className="font-bold md:text-sm">Accounts & Lists</p>
+          </div>
+
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-bold md:text-sm">& Orders</p>
+          </div>
+
+          <div className="flex items-center link">
+            <span className="absolute right-4 top-0 md:right-10 bg-yellow-600 h-4 w-4 text-center rounded-full text-black font-bold leading-[15px]">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="font-bold md:text-sm hidden md:inline mt-2">Cart</p>
+          </div>
         </div>
       </div>
 

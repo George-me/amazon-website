7992ab6ed16487d5a1583a2ce416427cfd3d93c2
George Mekhael <georgexp@live.com>
2024-10-06 18:08:15 +0400

    Created the banner carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </main>
     </div>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image from "next/image";
+import amazonbg1 from "../app/assets/1.jpg";
+import amazonbg2 from "../app/assets/2.jpg";
+import amazonbg3 from "../app/assets/2.jpg";
+import amazonbg4 from "../app/assets/2.jpg";
+import amazonbg5 from "../app/assets/2.jpg";
+
+const Banner = () => {
+  const banners = [amazonbg1, amazonbg2, amazonbg3, amazonbg4, amazonbg5];
+  return (
+    <div className="relative">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        {banners.map((banner, index) => (
+          <div>
+            <Image src={banner} alt={`Banner ${index + 1}`} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;

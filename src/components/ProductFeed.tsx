b7d5a62cfe88ab22f982427dcf34e9e4b520ccff
George Mekhael <georgexp@live.com>
2024-10-16 01:18:27 +0400

    Fetch products from api shortened

--- src/components/ProductFeed.tsx
+++ src/components/ProductFeed.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 
-const fetchProducts = async () => {
-  const response = await fetch("https://fakestoreapi.com/products").then(
-    (res) => res.json()
-  );
-
-  return response;
-};
-
 interface Products {
   title: string;
 }
 
 const ProductFeed = async () => {
-  const products = await fetchProducts();
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
 
   return (
     <div>
